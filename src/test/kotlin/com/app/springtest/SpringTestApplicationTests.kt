9c979aeaac7af47534a39815535d08091d258cd2
nabyungho <valoon2@naver.com>
2025-05-30 15:39:17 +0900

    [feat] init

---
+++ src/test/kotlin/com/app/springtest/SpringTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.springtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

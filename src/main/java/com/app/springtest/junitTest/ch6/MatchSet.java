e1dc20da4c572e01c60a5935fb1d765b8509ea8d
nabyungho <valoon2@naver.com>
2025-06-07 17:05:43 +0900

    [refactor] 사용하지 않는 메소드 삭제

--- src/main/java/com/app/springtest/junitTest/ch6/MatchSet.java
+++ src/main/java/com/app/springtest/junitTest/ch6/MatchSet.java
@@ -5,58 +5,56 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/utj2 for more book information.
-***/
+ ***/
 package com.app.springtest.junitTest.ch6;
 
 import lombok.Getter;
 
-import java.util.*;
-
 @Getter
 public class MatchSet {
-   private AnswerCollection answers;
-   private int score = 0;
-   private Criteria criteria;
+    private AnswerCollection answers;
+    private int score = 0;
+    private Criteria criteria;
 
-   public MatchSet(AnswerCollection answers, Criteria criteria) {
-      this.answers = answers;
-      this.criteria = criteria;
-   }
+    public MatchSet(AnswerCollection answers, Criteria criteria) {
+        this.answers = answers;
+        this.criteria = criteria;
+    }
 
-   public boolean getMatches() {
-      if (doesNotMatchAnyMustMatchCriterion()) {
-         return false;
-      }
-      return anyMatches();
-   }
+    public boolean getMatches() {
+        if (doesNotMatchAnyMustMatchCriterion()) {
+            return false;
+        }
+        return anyMatches();
+    }
 
-   private boolean doesNotMatchAnyMustMatchCriterion() {
-      for (Criterion criterion : criteria) {
-         boolean match = criterion.getMatches(answers.getAnswerMatching(criterion));
-         if (!match && criterion.getWeight() == Weight.MustMatch) {
-            return true;
-         }
-      }
-      return false;
-   }
+    public int getScore() {
+        int score = 0;
+        for (Criterion criterion : criteria) {
+            if (criterion.getMatches(answers.getAnswerMatching(criterion))) {
+                score += criterion.getWeight().getValue();
+            }
+        }
+        return score;
+    }
 
-   private boolean anyMatches() {
-      boolean anyMatches = false;
-      for (Criterion criterion : criteria) {
-         anyMatches |= criterion.getMatches(answers.getAnswerMatching(criterion));
-      }
-      return anyMatches;
-   }
+    private boolean doesNotMatchAnyMustMatchCriterion() {
+        for (Criterion criterion : criteria) {
+            boolean match = criterion.getMatches(answers.getAnswerMatching(criterion));
+            if (!match && criterion.getWeight() == Weight.MustMatch) {
+                return true;
+            }
+        }
 
-   private int getCalculateScore() {
-      int score = 0;
+        return false;
+    }
 
-      for (Criterion criterion : criteria) {
-         if (criterion.getMatches(answers.getAnswerMatching(criterion))) {
-            score += criterion.getWeight().getValue();
-         }
-      }
+    private boolean anyMatches() {
+        boolean anyMatches = false;
+        for (Criterion criterion : criteria) {
+            anyMatches |= criterion.getMatches(answers.getAnswerMatching(criterion));
+        }
+        return anyMatches;
+    }
 
-      return score;
-   }
 }

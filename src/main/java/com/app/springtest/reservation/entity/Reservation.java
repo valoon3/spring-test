b3deba0c8a8d9df6163c7769669adf47331233ef
nabyungho <valoon2@naver.com>
2025-06-11 15:30:01 +0900

    [feat] reservation 엔티티 생성

---
+++ src/main/java/com/app/springtest/reservation/entity/Reservation.java
@@ -0,0 +1,32 @@
+package com.app.springtest.reservation.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(
+        name = "reservation",
+        indexes = {
+
+        }
+)
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long userId;
+
+    @Column(nullable = false)
+    private long roomId;
+
+    @Embedded
+    private TimeSlice time;
+
+}

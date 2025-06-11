fde6eaa24d642234df05eafeb9b70986bfe41a91
nabyungho <valoon2@naver.com>
2025-06-11 15:32:08 +0900

    [feat] reservation repository 생성

---
+++ src/main/java/com/app/springtest/reservation/repository/ReservationRepository.java
@@ -0,0 +1,14 @@
+package com.app.springtest.reservation.repository;
+
+import com.app.springtest.reservation.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    Optional<Reservation> findByUserId(long userId);
+
+    Optional<Reservation> findByRoomId(long roomId);
+
+}

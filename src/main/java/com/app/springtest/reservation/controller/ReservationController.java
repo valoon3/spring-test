5abaaf891b8b79c710b35419b0604124b66c2cca
nabyungho <valoon2@naver.com>
2025-06-11 15:56:03 +0900

    [feat] 기본 controller 매핑

--- src/main/java/com/app/springtest/reservation/controller/ReservationController.java
+++ src/main/java/com/app/springtest/reservation/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package com.app.springtest.reservation.controller;
 
 import com.app.springtest.reservation.dto.ReservationRequestDto;
+import com.app.springtest.reservation.dto.ReservationResponseDto;
 import com.app.springtest.reservation.entity.Reservation;
 import com.app.springtest.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
@@ -15,20 +16,18 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    @PostMapping("/api/reservations")
+    @PostMapping()
     public void createReservation(@RequestBody final ReservationRequestDto request) {
         reservationService.createReservation(request);
     }
 
-    @GetMapping("/api/reservations/users/{userId}")
-    public List<Reservation> getUserReservations(@PathVariable final long userId) {
+    @GetMapping("/users/{userId}")
+    public List<ReservationResponseDto> getUserReservations(@PathVariable final long userId) {
         return reservationService.getUserReservations(userId);
     }
 
-    @GetMapping("/api/reservations/rooms/{roomId}")
-    public List<RoomReservation> getRoomReservations(@PathVariable final long roomId) {
-        /** !!!!!!! 해당 API는 절대 수정하지 마세요 !!!!!!! */
-        /** !!!!!!! 해당 회의실의 예약 현황을 조회 하기 위한 코드이니 수정하지 마세요 !!!!!!! */
-        return roomReservationRepository.findByRoomId(roomId);
-    }
+//    @GetMapping("/{roomId}")
+//    public List<ReservationResponseDto> getRoomReservations(@PathVariable final long roomId) {
+//        return roomReservationRepository.findByRoomId(roomId);
+//    }
 }
--- src/main/java/com/app/springtest/reservation/service/ReservationService.java
+++ src/main/java/com/app/springtest/reservation/service/ReservationService.java
@@ -1,8 +1,12 @@
 package com.app.springtest.reservation.service;
 
+import com.app.springtest.reservation.dto.ReservationRequestDto;
+import com.app.springtest.reservation.dto.ReservationResponseDto;
 import com.app.springtest.reservation.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +14,11 @@ public class ReservationService {
 
     private final  ReservationRepository roomReservationRepository;
 
+    public void createReservation(ReservationRequestDto request) {
+
+    }
+
+    public List<ReservationResponseDto> getUserReservations(long userId) {
+        return null;
+    }
 }
